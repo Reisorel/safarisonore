144e3edfbe84ffec5c08a61ccd0348a516b64e18
Lerosier <lerosier@MacBook-Air-de-Lerosier.local>
2025-12-27 18:55:26 +0100

    added dfinitive sounds

--- src/data/cardData.tsx
+++ src/data/cardData.tsx
@@ -1,53 +1,60 @@
-import Vache from "../assets/sounds/01_Vache.mp3";
-import Sidonie from "../assets/images/01_Sidonie.png"
-import Méroline from "../assets/images/02_Méroline.png"
-import Luminéoles from "../assets/images/03_Luminéoles.png"
-import Arkalon from "../assets/images/04_Arkalon.png"
-import Owéliane from "../assets/images/05_Owéliane.png"
-import Draviélor from "../assets/images/06_Draviélor.png"
-import Octalune from "../assets/images/07_Octalune.png"
+import Sidonie_sound from "../assets/sounds/01_Sidonie.mp3";
+import Méroline_sound from "../assets/sounds/02_Méroline.mp3";
+import Luminéoles_sound from "../assets/sounds/03_Luminéoles.mp3";
+import Arkalon_sound from "../assets/sounds/04_Arkalon.mp3";
+import Owéliane_sound from "../assets/sounds/05_Owéliane.mp3";
+import Draviélor_sound from "../assets/sounds/06_Draviélor.mp3";
+import Octalune_sound from "../assets/sounds/07_Octalune.mp3";
+
+import Sidonie_image from "../assets/images/01_Sidonie.png"
+import Méroline_image from "../assets/images/02_Méroline.png"
+import Luminéoles_image from "../assets/images/03_Luminéoles.png"
+import Arkalon_image from "../assets/images/04_Arkalon.png"
+import Owéliane_image from "../assets/images/05_Owéliane.png"
+import Draviélor_image from "../assets/images/06_Draviélor.png"
+import Octalune_image from "../assets/images/07_Octalune.png"
 
 export const cardData = [
   {
     id: "Sidonie",
     name: "Sidonie",
-    sound: Vache,
-    image: Sidonie,
+    sound: Sidonie_sound,
+    image: Sidonie_image,
   },
   {
     id: "Méroline",
     name: "Méroline",
-    sound: Vache,
-    image: Méroline,
+    sound: Méroline_sound,
+    image: Méroline_image,
   },
   {
     id: "Les Luminéoles",
     name: "Les Luminéoles",
-    sound: Vache,
-    image: Luminéoles,
+    sound: Luminéoles_sound,
+    image: Luminéoles_image,
   },
   {
     id: "Arkalon",
     name: "Arkalon",
-    sound: Vache,
-    image: Arkalon,
+    sound: Arkalon_sound,
+    image: Arkalon_image,
   },
   {
     id: "Owéliane",
     name: "Owéliane",
-    sound: Vache,
-    image: Owéliane,
+    sound: Owéliane_sound,
+    image: Owéliane_image,
   },
   {
     id: "Dravélior",
     name: "Dravélior",
-    sound: Vache,
-    image: Draviélor,
+    sound: Draviélor_sound,
+    image: Draviélor_image,
   },
   {
     id: "L'Octalune",
     name: "L'Octalune",
-    sound: Vache,
-    image: Octalune,
+    sound: Octalune_sound,
+    image: Octalune_image,
   },
 ];
